4061419daba86831a335819ca872f39a980d8cc7
Mayur B Patel <mayurpatel98765@gmail.com>
2023-09-15 11:19:00 +0530

    Change : default enviornment setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 const environment = {
   production: process.env.REACT_APP_PRODUCTION === 'true',
-  baseUrl: 'https://dev-domain-name/api/' // develop Base URL
+  baseUrl: '#{BASE_URL}#' // develop Base URL
   // client_id: process.env.REACT_APP_CLIENT_ID as string,
   // client_secret: process.env.REACT_APP_CLIENT_SECRET as string,
 };
